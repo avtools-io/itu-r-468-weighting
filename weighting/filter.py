86c20e85b92b36ce6a0e449b74d35e3fd34058cc
Alexis Michaltsis <alexis@michaltsis.net>
2019-08-17 14:18:50 +0200

    Add docstring to r468 function

--- weighting/filter.py
+++ weighting/filter.py
@@ -4,18 +4,45 @@ from frequencies import ITU_R_468_FREQUENCIES
 
 
 def r468(f, kHz=1):
+    """Takes a frequency value and returns a weighted dB value.
+
+    For weightening, the ITU-R-468 standard is followed.
+
+    The math for this is taken from Wikipedia (as of 2019-08-08):
+
+    https://en.wikipedia.org/wiki/ITU-R_468_noise_weighting
+
+    Shortened link to the archived Wikipedia page (as of 2019-08-08):
+
+    https://bit.ly/2P29qOh
+
+    Parameters
+    ----------
+    f : float
+        frequency, should be from 1.0 to 96000.0
+    kHz : int
+        choose 1kHz or 2kHz filtering, possible values: 1 or 2 (default 1)
+
+    Returns
+    -------
+    float
+        The dB weighted value of the frequency
+    """
+
     f2, f3, f4, f5, f6 = f**2, f**3, f**4, f**5, f**6
     h1_1, h1_2, h1_3 = -4.737338981378384, 2.043828333606125, 1.363894795463638
     h1 = h1_1*(10**-24)*f6 + h1_2*(10**-15)*f4 - h1_3*(10**-7)*f2 + 1
     h2_1, h2_2, h2_3 = 1.306612257412824, 2.118150887518656, 5.559488023498642
     h2 = h2_1*(10**-19)*f5 - h2_2*(10**-11)*f3 + h2_3*(10**-4)*f
     rITU = (1.246332637532143*(10**-4)*f) / sqrt(h1**2 + h2**2)
+
     if kHz == 1:
         gain = 18.2
     elif kHz == 2:
         gain = 12.6
     else:
         raise ValueError
+
     return gain + 20 * log10(rITU)
 
 
