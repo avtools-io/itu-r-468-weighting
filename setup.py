bd94301e8c2e9b8bcc5fd182334021a82429bd3b
Alexis Michaltsis <alexis@michaltsis.net>
2019-09-15 00:33:29 +0200

    Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="itu-r-468-weighting",
-    version="1.1.1",
+    version="1.1.2",
     description="A zero dependency Python ITU-R 468 noise weighting filter (1 kHz and 2 kHz)",
     long_description=long_description,
     long_description_content_type="text/markdown",
